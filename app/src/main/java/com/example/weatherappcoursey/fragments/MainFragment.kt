85fcd40772628629557ba2d73b7855a23bd58ca0
Romashkashtyr <Romainshtyr@yandex.ru>
2024-01-16 14:04:31 +0300

    Fragment_main Well Done

--- app/src/main/java/com/example/weatherappcoursey/fragments/MainFragment.kt
+++ app/src/main/java/com/example/weatherappcoursey/fragments/MainFragment.kt
@@ -1,16 +1,21 @@
 package com.example.weatherappcoursey.fragments
 
+import android.Manifest
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.weatherappcoursey.R
 import com.example.weatherappcoursey.databinding.FragmentMainBinding
+import java.lang.ref.ReferenceQueue
 
 
 class MainFragment : Fragment() {
-
+    private lateinit var pLauncher: ActivityResultLauncher<String>
     private lateinit var binding: FragmentMainBinding
 
 
@@ -21,6 +26,26 @@ class MainFragment : Fragment() {
     ): View {
         binding = FragmentMainBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        checkPermission()
+    }
+
+    private fun permissionListener() {
+        pLauncher = registerForActivityResult(
+            ActivityResultContracts.RequestPermission()) {
+            Toast.makeText(activity, "Permission is $it", Toast.LENGTH_LONG).show()
+        }
+    }
+
+
+    private fun checkPermission(){
+        if(!isPermissionGranted(Manifest.permission.ACCESS_FINE_LOCATION)){
+            permissionListener()
+            pLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+        }
     }
 
     companion object {
